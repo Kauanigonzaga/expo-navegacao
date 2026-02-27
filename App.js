3b924a421545727f1532f92732b5a3b2bce2a2f9
Kauani Nunes Costa Gonzaga de Melo <nuneskauani47@gmail.com>
2026-02-27 20:30:33 -0300

    teste

--- App.js
+++ App.js
@@ -27,9 +27,9 @@ import Atividade07 from './src/atividades/atv07';
 
 // import Revisao01 from './src/revisao/rev01';
 
-import Revisao02 from './src/revisao/rev02';
-import Revisao03 from './src/revisao/rev03';
-import Revisao09 from './src/revisao/rev09';
+// import Revisao02 from './src/revisao/rev02';
+// import Revisao03 from './src/revisao/rev03';
+// import Revisao09 from './src/revisao/rev09';
 import Navegacao from './src/revisao/navegacao';
 
 export default function App() {
